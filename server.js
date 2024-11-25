5235fc9efa21e09c0bea6c68a6dcadea8da6f990
Skylabhub <davidebarletta1234@gmail.com>
2024-11-25 18:02:32 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -62,6 +62,8 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000; // Usa la porta fornita dal servizio di hosting o la porta 3000
-server.listen(PORT, () => {
-    console.log(`Server avviato su http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server avviato su http://0.0.0.0:${PORT}`);
+});
+
 });
